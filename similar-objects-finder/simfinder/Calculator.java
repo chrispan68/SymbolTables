b2574ad1e624d772061dc84144fa3ee3e0af8a19
Walorus <pvass153@gmail.com>
2016-02-16 14:57:00 -0500

    wrote the calculate method

--- similar-objects-finder/simfinder/Calculator.java
+++ similar-objects-finder/simfinder/Calculator.java
@@ -27,9 +27,25 @@ public class Calculator {
 
     /** Awards points to schools that appear in the data for the given
 	FieldValues */
+
+    //Goes through each of the 'i' inputObjects in data and through each of their 'j' attributes checking if the target is equal to it
+    //If it is, it adds a point to the hashmap, depending on whether the Id exists in the hashmap or not it will put it new at 1 point, or
+    //add onto the previous existing points.
     public Map<String, Float> calculate(FieldValue[] target) {
     	Map<String, Float> points = new HashMap<String, Float>();
-
+        for(int i =0;i<data.size();i++){
+            String currentId = data.get(i).getId();
+            FieldValue [] currentData = data.get(i).getFields();
+            for(int j=0;j<data.get(i).getFields().length;j++){
+                if(currentData[j].equals(target[j])){
+                    if(points.containsKey(currentId)){
+                        points.put(currentId,points.get(currentId)+1); //1 is random number of points that is added if it matches, not sure what 
+                    }else{                                               //we want to do
+                        points.put(currentId,1);
+                    }
+                }
+            }
+        }
     	
     	
     	return points;
