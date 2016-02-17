aaea178c7e712ddd208ac7d9000eb21002f4c6f5
Walorus <pvass153@gmail.com>
2016-02-17 08:05:34 -0500

    Realized I messed some things up so put a note of it

--- similar-objects-finder/simfinder/Calculator.java
+++ similar-objects-finder/simfinder/Calculator.java
@@ -28,15 +28,15 @@ public class Calculator {
     /** Awards points to schools that appear in the data for the given
 	FieldValues */
 
-    //Goes through each of the 'i' inputObjects in data and through each of their 'j' attributes checking if the target is equal to it
+    //Goes through each of the 'i' keys in data and through each of their 'j' attributes checking if the target is equal to it
     //If it is, it adds a point to the hashmap, depending on whether the Id exists in the hashmap or not it will put it new at 1 point, or
     //add onto the previous existing points.
     public Map<String, Float> calculate(FieldValue[] target) {
     	Map<String, Float> points = new HashMap<String, Float>();
         for(int i =0;i<data.size();i++){
-            String currentId = data.get(i).getId();
-            FieldValue [] currentData = data.get(i).getFields();
-            for(int j=0;j<data.get(i).getFields().length;j++){
+            String currentId = data.get(i).getId(); //INCORRECT, did incorrectly assuming data was an arrayList
+            FieldValue [] currentData = data.get(i).getFields(); //INCORRECT, did incorrectly assuming data was an arrayList
+            for(int j=0;j<data.get(i).getFields().length;j++){ //INCORRECT, did incorrectly assuming data was an arrayList
                 if(currentData[j].equals(target[j])){
                     if(points.containsKey(currentId)){
                         points.put(currentId,points.get(currentId)+1); //1 is random number of points that is added if it matches, not sure what 
