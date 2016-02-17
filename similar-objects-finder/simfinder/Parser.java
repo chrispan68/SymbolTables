8c6f800ce200d3cedc4fde11564eccaf5cf6f060
Walorus <pvass153@gmail.com>
2016-02-17 13:27:47 -0500

    Undid changes to parser

--- similar-objects-finder/simfinder/Parser.java
+++ similar-objects-finder/simfinder/Parser.java
@@ -5,28 +5,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Parser {
-	
+    
     public static ArrayList<InputObject> parseFile(File file) throws IOException {
         ArrayList<InputObject> data = new ArrayList<InputObject>();
-    	FileReader in = new FileReader(file);
-    	BufferedReader br = new BufferedReader(in);
-    	data = new ArrayList<InputObject>();
-    	String[] headers = arraySlice(br.readLine().split("\t"));
-	String line;
-    	String[] currentLine;
-    	while ((line = br.readLine()) != null) {
-	    currentLine = line.split("\t");
-	    String id = currentLine[0];
-	    String [] values = arraySlice(currentLine);
-	    data.add(new InputObject(id,headers,values));
-	}
-    	in.close();
+        FileReader in = new FileReader(file);
+        BufferedReader br = new BufferedReader(in);
+        data = new ArrayList<InputObject>();
+        String[] headers = arraySlice(br.readLine().split("\t"));
+        String line;
+        String[] currentLine;
+        while ((line = br.readLine()) != null) {
+        currentLine = line.split("\t");
+        String id = currentLine[0];
+        String [] values = arraySlice(currentLine);
+        data.add(new InputObject(id,headers,values));
+    }
+        in.close();
 
         return data;
     }
 
     private static String[] arraySlice(String[] s) {
-	return Arrays.copyOfRange(s, 1, s.length);
+    return Arrays.copyOfRange(s, 1, s.length);
     }
     
 }
