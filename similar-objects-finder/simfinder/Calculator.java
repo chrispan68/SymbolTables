ddd1130fecb1ed15e39d0dd6c7f8b07837c55a91
Walorus <pvass153@gmail.com>
2016-03-02 12:02:29 -0500

    Merge?

--- similar-objects-finder/simfinder/Calculator.java
+++ similar-objects-finder/simfinder/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator {
     private Map<FieldValue, Set<String>> data;
     private Map<String, Set<String>> options;
     private String[] fields;
+    private String[] ids;
     
     /** Takes a mapping of object names/ids to mapping of
 	Should fill out the fields and data variables. */
@@ -14,11 +15,18 @@ public class Calculator {
 	data = new HashMap<FieldValue, Set<String>>();
 	options = new HashMap<String, Set<String>>();
 	Set<String> fieldset = new HashSet<String>(input.get(0).getFields().length);
-	for(InputObject i : input) {
+	ids = new String[input.size()];
+	// For each line of input
+	for(int j = 0; j < input.size(); j++) {
+	    InputObject i = input.get(j);
+	    ids[j] = input.get(j).getId();
+	    // For each item on that line
 	    for (FieldValue f : i.getFields()) {
+		// If the data-value pair is new, create it
 		if (!data.containsKey(f)) {
 		    data.put(f, new HashSet<String>(input.size() / 2));
 		}
+		// Add the InputObject id to the Set for the data-value pair
 		data.get(f).add(i.getId());
 		fieldset.add(f.getHeader());
 		// Add to options Map
@@ -34,53 +42,42 @@ public class Calculator {
 
     /** Awards points to schools that appear in the data for the given
 	FieldValues */
-    public ArrayList<String> calculate(FieldValue[] target,int numOfResults) {
+    public Map.Entry[] calculate(FieldValue[] target,int numOfResults) {
     	Map<String, Float> points = new HashMap<String, Float>();
-        for(int i =0; i<target.length;i++){ //Goes through each of the fieldValues for the clients target attributes
-            Set<String> matchedIds = data.get(target[i]); //Gets the set of IDs of places which have the current fieldValue 
-            for(String currentId: matchedIds){            //value (for example, all which have temperature value of cold.)
-                if(points.containsKey(currentId)){ //If the college has already been added, adds on 1 point
-                        points.put(currentId,points.get(currentId)+1.0f); 
-                }else{                                               //if it hasn't creates it with 1 point.
-                    points.put(currentId,1.0f);
-                }
-                
+	// Goes through each of the fieldValues for the client's
+	// target attributes
+	for (String id : ids) {
+	    points.put(id, 0.0f);
+	}
+        for(int i =0; i<target.length;i++){
+	    // Gets the set of IDs of places which have the current fieldValue 
+            Set<String> matchedIds = data.get(target[i]);
+            // value (for example, all which have temperature value of cold.)
+            for(String currentId: matchedIds){
+		// adds on 1 point
+		points.put(currentId,points.get(currentId)+1.0f);
             }
         }
-    	return setMaxes(points,numOfResults);
+    	return getMaxes(points,numOfResults);
     }
 
-    private ArrayList<String> setMaxes(Map<String,Float> points,int numOfResults){
-        String [] allIds = (String[])points.keySet().toArray(new String[points.size()]); //changes all the keys of the hashmap into a String array
-        ArrayList<String> results = new ArrayList<String>();
-        for(int j =0;j<points.size();j++){ //going through the IDs from the point hashmap
-            Float rPoints = 0f, cPoints = points.get(allIds[j]); //sets the current point value from the current item on the points hashmap
-            int count = 0;
-            boolean stillNeedsToAdd = true;
-            while(count+1<numOfResults && stillNeedsToAdd){ // makes it so if we have the number of results requested, it stop checking if it's greater.
-                if(count==results.size()){ //If the results is < the required num and we reach the end of the results,just add it to the end.
-                    results.add(allIds[j]);
-                    stillNeedsToAdd = false;
-                }
-                rPoints = points.get(results.get(count)); //if we aren't at the end of results, get the result points to compare
-                if(cPoints >= rPoints && stillNeedsToAdd){
-                    results.add(count,allIds[j]); //if the current points are greater than res points, add current points to the spot.
-                    stillNeedsToAdd = false;
-                }
-                if(results.size()>numOfResults){ //Trims it down so we don't have more than the requested number of results in the arrayList
-                    results.remove(results.size()-1);
-                }
-                count++;
-            }   
-        }
-        for(int i =0;i<results.size();i++){ //Just a little code to make sure it's working, adds on the results to the points. Can remove later.
-            results.set(i,results.get(i)+" - "+points.get(results.get(i)));
-        }
-
-        return results;
+    private class PointsComparator implements Comparator<Map.Entry<String, Float>> {
+	public int compare(Map.Entry<String, Float> o1, Map.Entry<String, Float> o2) {
+	    return (int)(o2.getValue() - o1.getValue());
+	}
+    }
+    
+    private Map.Entry[] getMaxes(Map<String,Float> points){
+	Map.Entry<String, Float>[] entries = new Map.Entry[points.size()];
+	points.entrySet().toArray(entries);
+	Arrays.sort(entries, new PointsComparator());
+	return entries;
     }
 
-
+    private Map.Entry[] getMaxes(Map<String, Float> points, int numOfResults) {
+	return Arrays.copyOf(getMaxes(points), Math.min(numOfResults, points.size()));
+    }
+    
     /** Return an array of blank FieldValues, copies from fields. */
     public FieldValue[] getBlankFields() {
 	FieldValue[] a = new FieldValue[fields.length];
--- similar-objects-finder/simfinder/Driver.java
+++ similar-objects-finder/simfinder/Driver.java
@@ -8,24 +8,25 @@ public class Driver {
 		File f = new File(args[0]);
 		Calculator c = new Calculator(Parser.parseFile(f));
 		FieldValue[] blankFields = c.getBlankFields();
-		System.out.println("Please choose your preference for the following attribute.");
+		System.out.println("Please choose your preference for the following attributes.");
 		int i=0;
 		while(i<blankFields.length){
 			String[] options = c.getOptionsFor(blankFields[i].getHeader());
-			System.out.println("Options for "+blankFields[i].getHeader()+": ");
+			System.out.print("Choose "+blankFields[i].getHeader()+" (");
 			for(String option: options){
-				System.out.println(option);
+				System.out.print(option + ", ");
 			}
+			System.out.print("\b\b): ");
 			String input = getUserInput();
 			blankFields[i].setValue(input);
 			if(Arrays.asList(options).contains(input))
 				i++;
 
 		}
-		System.out.println("Number of returned results?");
+		System.out.print("Number of returned results? ");
 		int numOfResults = Integer.parseInt(getUserInput());
 
-		ArrayList<String> results = c.calculate(blankFields,numOfResults);
+		Map.Entry[] results = c.calculate(blankFields,numOfResults);
 		print(results);
 	}
 
@@ -34,9 +35,10 @@ public class Driver {
         return sc.nextLine();
 
     }
-    public static void print(ArrayList<String> res){ //Used to print the results from an arrayList
-    	for(int i =0;i<res.size();i++){
-    		System.out.println((i+1)+". "+res.get(i));
+    public static void print(Map.Entry[] res){ //Used to print the results from an arrayList
+    	for(int i =0;i<res.length;i++){
+	    System.out.print((i+1)+". "+res[i].getKey());
+	    System.out.println(" - " + res[i].getValue());
     	}
     }
 }
